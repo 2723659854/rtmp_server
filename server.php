aca7f1a0633511e921e3a60f1e82a53cd1ee66d6
yanglong <2723659854@qq.com>
2024-06-19 09:32:11 +0800

    还原端口为80

--- server.php
+++ server.php
@@ -9,7 +9,7 @@ try {
     /** 设置flv通信端口 可以自行修改 默认8501 */
     $server->flvPort = 8501;
     /** hls协议预留端口 8000 */
-    $server->webPort = 8000;
+    $server->webPort = 80;
     /** 启动服务 */
     $server->start();
 }catch (Exception $exception){
